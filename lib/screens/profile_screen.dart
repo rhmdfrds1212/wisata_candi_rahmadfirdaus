1625b43e6b2c812fab221bbd72e1037274489988
rahmad firdaus <163318781+rhmdfrds1212@users.noreply.github.com>
2024-10-26 09:24:23 +0700

    update profil screen

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -21,36 +21,45 @@ class _ProfileScreenState extends State<ProfileScreen> {
             color: Colors.deepPurple,
           ),
           Padding(
-            padding: const EdgeInsets.only(top: 200-50),
+            padding: const EdgeInsets.symmetric(horizontal: 16),
             child: Column(
               children: [
                 //   TODO 2. Buat bagian ProfileHeader yang berisi gambar profil
-                Stack(
-                  alignment: Alignment.bottomRight,
-                  children: [
-                    Container(
-                      decoration: BoxDecoration(
-                        border: Border.all(color: Colors.deepPurple, width: 2),
-                        shape: BoxShape.circle,
+                Align(
+                  alignment: Alignment.topCenter,
+                  child: Padding(
+                      padding: const EdgeInsets.only(top: 200-50),
+                      child: Stack(
+                        alignment: Alignment.bottomRight,
+                        children: [
+                          Container(
+                            decoration: BoxDecoration(
+                              border: Border.all(color: Colors.deepPurple, width: 2),
+                              shape: BoxShape.circle,
+                            ),
+                            child: CircleAvatar(
+                              radius: 50,
+                              backgroundImage:
+                                  AssetImage('images/placeholder_image.png'),
+                            ),
+                          ),
+                          if(isSignedIn)
+                            IconButton(
+                                onPressed: (){},
+                                icon: Icon(Icons.camera_alt, color: Colors.deepPurple[50],),
+                            ),
+                        ],
                       ),
-                      child: CircleAvatar(
-                        radius: 50,
-                        backgroundImage: AssetImage('images/placeholder_image.png'),
-                      ),
+                      //   TODO 3. Buaqt bagian ProfileInfo yang berisi info profile
+                      //   TODO 4. buat ProfileActions yang berisi TextButton sign in/out
                     ),
-                    if(isSignedIn)
-                      IconButton(
-                          onPressed: (){},
-                          icon: Icon(Icons.camera_alt, color: Colors.deepPurple[50],),),
-                  ],
-                ),
-                //   TODO 3. Buaqt bagian ProfileInfo yang berisi info profile
-                //   TODO 4. buat ProfileActions yang berisi TextButton sign in/out
+                  ),
               ],
             ),
-          )
+          ),
         ],
       ),
     );
   }
 }
+
