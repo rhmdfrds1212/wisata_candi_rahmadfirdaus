412150b0c2aabd652c37f5af66e988c77008d176
CalistaVania <163318781+rhmdfrds1212@users.noreply.github.com>
2024-11-04 13:06:49 +0700

    update sign in sign up

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:wisata_candi/screens/sign_in_screen.dart';
 import 'screens/profile_screen.dart';
 import 'data/candi_data.dart';
 import 'package:wisata_candi/screens/detail_screen.dart';
@@ -16,11 +17,24 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Wisata Candi',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        appBarTheme: const AppBarTheme(
+          iconTheme: IconThemeData(color: Colors.deepPurple),
+          titleTextStyle: TextStyle(
+            color: Colors.deepPurple,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        colorScheme:
+        ColorScheme.fromSeed(seedColor: Colors.deepPurple).copyWith(
+          primary: Colors.deepPurple,
+          surface: Colors.deepPurple[50],
+        ),
         useMaterial3: true,
       ),
-      home: ProfileScreen(),
+      // home: const ProfileScreen(),
       // home: DetailScreen(candi: candiList[0]),
+      home: SignInScreen(),
     );
   }
 }
